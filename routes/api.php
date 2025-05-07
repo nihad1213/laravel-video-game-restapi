6cfef2be4c67674c096fdc1990299f7e91df664f
Nihad <nihad.nemetli@gmail.com>
2025-05-07 11:14:46 +0400

    tymon/jwt instaleed

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\GameController;
+use App\Http\Controllers\Api\PublisherController;
+use App\Http\Controllers\Api\GenreController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('games', GameController::class);
+    Route::apiResource('publishers', PublisherController::class);
+    Route::apiResource('genres', GenreController::class);
+});
