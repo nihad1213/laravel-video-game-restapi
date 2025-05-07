ccf375f7950b740b2d93cf43538b668a9a5d051b
Nihad <nihad.nemetli@gmail.com>
2025-05-07 11:12:35 +0400

    Publisher Controller's methods fixed

--- app/Http/Controllers/Api/PublisherController.php
+++ app/Http/Controllers/Api/PublisherController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Publisher;
 
 class PublisherController extends Controller
 {
@@ -12,7 +13,7 @@ class PublisherController extends Controller
      */
     public function index()
     {
-        //
+        return Publisher::with('games')->get();
     }
 
     /**
@@ -20,30 +21,46 @@ class PublisherController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'website' => 'nullable|url',
+            'founded_year' => 'nullable|integer',
+        ]);
+
+        return Publisher::create($data);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Publisher $publisher)
     {
-        //
+        return $publisher->load('games');
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Publisher $publisher)
     {
-        //
+        $data = $request->validate([
+            'name' => 'sometimes|required|string|max:255',
+            'website' => 'sometimes|nullable|url',
+            'founded_year' => 'sometimes|nullable|integer',
+        ]);
+
+        $publisher->update($data);
+
+        return $publisher;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Publisher $publisher)
     {
-        //
+        $publisher->delete();
+
+        return response()->json(null, 204);
     }
 }
